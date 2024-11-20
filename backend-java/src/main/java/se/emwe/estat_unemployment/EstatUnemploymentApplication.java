914ce742e2b5764d2f507c9742e0e52916f78f3e
Emil Wennström <emilwennstrom@gmail.com>
2024-11-20 01:31:24 +0100

    java update

---
+++ backend-java/src/main/java/se/emwe/estat_unemployment/EstatUnemploymentApplication.java
@@ -0,0 +1,15 @@
+package se.emwe.estat_unemployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstatUnemploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstatUnemploymentApplication.class, args);
+	}
+
+}
+
+
